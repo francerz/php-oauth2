dbb30ab1a938a0450e3e5958ed4acc1d4690160e
Francerz <francerzam@hotmail.com>
2020-10-19 16:15:46 -0500

    Updated AccessToken test

--- tests/AccessTokenTest.php
+++ tests/AccessTokenTest.php
@@ -7,13 +7,14 @@ class AccessTokenTest extends TestCase
 {
     public function testInstantiation()
     {
-        $at = new AccessToken('abcdefgh', 'Bearer', 3600, 'ijklmnop', 10000);
+        $at = new AccessToken('abcdefgh', 'Bearer', 3600, 'ijklmnop', 10000, 'scp1 scp2');
 
         $this->assertEquals('abcdefgh', $at->getAccessToken());
         $this->assertEquals('Bearer',   $at->getTokenType());
         $this->assertEquals(3600,       $at->getExpiresIn());
         $this->assertEquals('ijklmnop', $at->getRefreshToken());
         $this->assertEquals(10000,      $at->getCreateTime());
+        $this->assertEquals('scp1 scp2',$at->getScope());
 
         return $at;
     }
